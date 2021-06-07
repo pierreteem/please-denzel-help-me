11459ad11c3f0928b9aea570b9c1173b2efa2575
pierreteem <78645343+pierreteem@users.noreply.github.com>
2021-06-07 10:42:45 +0200

    git push origin master

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Q1. Create a variable called `denzel` - that can't be redeclared - and assign it the string 'please denzel, help me'
+const denzel ='please denzel, help me';
 
 // Q2. Use the variable `actor` to create a new variable called `inspirational` that is assigned the string "Is Denzel Washington an inspirational actor?"
 const actor = 'Denzel Washington';
+const inspirational = 'Is ' + actor + ' an inspirational actor?';
 
 // Q3  Create an object called `family` and give it the following properties:
 // family has been `married` on "1983" (Number)
 // family is not `presidentOfTheUnitedStates` (boolean)
 // family has `children` called "John David", "Katia", "Olivia" and "Malcolm" (array of strings)
+family = {'married':1983, 'presidentOfTheUnitedStates':false, 'children':['John David', 'Katia', 'Olivia', 'Malcolm']};
 
 // Q4. Change
 // * the `name` stage to "A Raisin in the Sun"
@@ -16,11 +19,14 @@ const stage = {
   'name': 'Fences',
   'year': '2010'
 };
+stage.name = 'A Raisin in the Sun';
+stage.year = 2014;
 
 // Q5. Change the `movie` "glory" to "Training Day" using square bracket notation
 const awards = {
   'movie': 'glory'
 };
+awards['movie'] = 'Training Day';
 
 // Q6. Create an object called "earlywork"
 // The key should be the movie name and the value the release year
@@ -30,29 +36,38 @@ const awards = {
 // * Power (1986)
 // * Cry Freedom (1987)
 // * For Queen and Countryy (1988)
+const earlywork = {'Carbon Copy':1981, 'A Soldier Story':1984, 'Power':1986, 'Cry Freedom':1987, 'For Queen and Coutryy':1988};
 
 // Q7. Return a new array from `debaters` with all item in uppercase
 const debaters = ['we do', 'what we have to do', 'in order to do', 'what we want to do'];
-
+const debatersBig = debaters.toUpperCase();
 // Q8. Using this array.
 // Do the following 5 steps
 const directors = ['spikelee', 'ridleyscott', 'zemeckis'];
 
 // 1. add "tonyscott" value to the end of `directors` array
+directors.push('tonyscott');
 
 // 2. remove "spikelee" value and store it in a variable called firstDirector
+const firstDirector = directors.shift();
 
 // 3. add "himself" value to the start of `directors` array
+directors.unshift('himself');
 
 // 4. remove "ridleyscott" value from the array and store it in a variable called secondDirector
+const secondDirector = directors.splice(1,1);
 
 // 5. let 'zemeckis' in the array but put a copy of it on a variable called thirdDirector
-
+const thirdDirector = directors[1];
 
 // Q9. Write the function `duplicate` that return the expected result
 console.log(duplicate(['Happily', 'Ever', 'After', 'Fairy', 'Tales', 'for', 'Every', 'Child']));
 // ❯ ['Happily', 'Ever', 'After', 'Fairy', 'Tales', 'for', 'Every', 'Child', 'Happily', 'Ever', 'After', 'Fairy', 'Tales', 'for', 'Every', 'Child']
-
+function duplicate(arrayString){
+  const result = arrayString;
+  arrayString.forEach(element => result.push(element));
+  return result;
+}
 
 // Q10. Refactor the current ES5-style function `police` in ES6-style.
 const police = function (names) {
@@ -108,11 +123,12 @@ fetch('http://api.tvmaze.com/search/people?q=denzel+washington')
   .then(console.log);
 
 // Q16. Give me at least 3 memorable websites that engage to continue (because of nice UX/UI AND avoid to give me facebook, airbnb etc...)
-
+websites =  ['coursera.org', 'irrationallabs.com', 'iim.fr']; 
 // Q17. Describe an ESILV project that you worked on that you’re proud of?
-
+const proud = 'I took part in a project in partnership with Miscrosoft with the aim of helping dyslexic people. Fascinating, difficult, engaging.';
 // Q18. Describe an ESILV project that you worked on that you’re not so proud of?
-
+const proudNot = 'A project where we had to build some small robot. We tried a way different than most and the result were disappointing. We were in the last quarter';
 // Q19. What are some things you like about the developer tools you use?
-
+const tools = 'The rapid feedback they provide and the simple interface. Not much GUI but efficient and effective results.';
 // Q20. Last question: could you explain me - in your terms - why the title of the course is "Web Application Architecture(s)" and not "Web Application Architecture"?
+const last = 'There are more than one architecture, more than one way web app components can interact. Hence the plural.';
